8e21e05718e62ab9198f455a36fcd4aa048a3630
Munna Ahmed <munnaahmed2025@gmail.com>
2023-01-16 03:01:59 +0600

    added redux middleware

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -2,7 +2,12 @@ import { applyMiddleware, createStore } from "redux";
 import { composeWithDevTools } from "@redux-devtools/extension";
 import rootReducer from "./reducers/rootReducer";
 import logger from "redux-logger";
+import cartCounter from "./middlewares/cartCounter";
+import thunk from "redux-thunk";
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware()));
+const store = createStore(
+    rootReducer,
+    composeWithDevTools(applyMiddleware(cartCounter, thunk, logger))
+);
 
 export default store;
